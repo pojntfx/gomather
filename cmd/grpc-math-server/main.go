0d2f90f7ff0a7a847ef2f49a82bbbcf994f8a81a
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-10-22 00:25:52 +0200

    feat: Add initial implementation

---
+++ cmd/grpc-math-server/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net"
+
+	math "github.com/pojntfx/grpc-math/lib/proto"
+	"github.com/pojntfx/grpc-math/lib/svc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+// The port to run the gRPC server on
+var port = flag.String("port", ":30000", "The port to run the gRPC server on")
+
+func main() {
+	// Parse the flags
+	flag.Parse()
+
+	// Start the listener
+	listener, err := net.Listen("tcp", *port)
+	if err != nil {
+		log.Fatalln("server could not listen", err)
+	}
+
+	// Start the server
+	server := grpc.NewServer()
+
+	// Register the services
+	reflection.Register(server)
+	math.RegisterMathServer(server, &svc.Math{})
+	log.Println("server started on port", *port)
+
+	// Serve the server via the listener
+	err = server.Serve(listener)
+	if err != nil {
+		log.Fatalln("server could not start", err)
+	}
+}
